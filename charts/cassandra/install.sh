efc4b3f4d87fb62291267f0cd1724adf2a6cb17e
Antonio Ciolino <antonio.ciolino@gmail.com>
2020-10-06 14:16:40 -0400

    split the chart files, added upgrade -f

--- charts/cassandra/install.sh
+++ charts/cassandra/install.sh
@@ -2,4 +2,7 @@ export HELM_NAMESPACE=cassandra
 helm package cassandra
 # first time install. upgrades are in the notes file.
 # do NOT do helm delete as it removes all the nodes.
-helm install monetization-cassandra ./cassandra --namespace cassandra
+# helm install monetization-cassandra ./cassandra --namespace cassandra
+
+#uses files for configs - cleaner implementation
+helm upgrade -f cassandra/values-stage-central.yaml monetization-cassandra cassandra -n cassandra
