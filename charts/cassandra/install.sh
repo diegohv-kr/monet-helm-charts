a363464adc47c1c4bf0fbfe1a5bf0e45cb54273b
dou4316 <ricardo.zaragoza@kroger.com>
2023-11-16 20:58:38 -0600

    Update CPUs request to 10 on GCP Prod region east

--- charts/cassandra/install.sh
+++ charts/cassandra/install.sh
@@ -6,7 +6,7 @@ helm package cassandra
 
 #uses files for configs - cleaner implementation
 # helm upgrade -f cassandra/values.yaml monetization-cassandra cassandra -n cassandra --dry-run
-helm upgrade -f cassandra/values-prod-central-rpd.yaml monetization-cassandra cassandra -n cassandra
+helm upgrade -f cassandra/values-prod-east-rpd.yaml monetization-cassandra cassandra -n cassandra
 # helm upgrade --set config.cluster_size=4 --set persistence.size=5Gi monetization-cassandra cassandra -n cassandra
 
 # we can restart the replicas of StatefulSet to take last changes through kubectl
